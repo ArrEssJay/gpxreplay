8bf434aa7100a5fcae19ba107a6ecc56d2935e3a
Priit Haamer <priit@haamer.ee>
2017-10-23 14:17:08 +0300

    Add TypeScript type definitions

---
+++ gpxreplay.d.ts
@@ -0,0 +1,32 @@
+declare module 'gpxreplay' {
+
+  interface Point {
+    lon: number;
+    lat: number;
+    elevation: number;
+    fromPrevious: {
+      bearing: number;
+      veolocity: number;
+      time: number;
+      climb: number;
+      distance: number
+    };
+    toCurrent: {
+      time: number;
+      meanVelocity: number;
+      climb: number;
+      distance: number
+    };
+  }
+
+  type WaypointCallback = (point: Point) => void;
+
+  interface GPXReplayOptions {
+    rate: number;
+    loop: boolean;
+    limitWait: number;
+    precision: number;
+  }
+
+  export const playTrack: (trackFile: string, printWaypoint: WaypointCallback, options: GPXReplayOptions) => void;
+}
